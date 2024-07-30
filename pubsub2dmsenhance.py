bfc59b321e9c31c2b2b9e13fdee0ebcb6be4cd64
何晨 <hecncn@126.com>
2024-07-30 17:09:04 +0800

    Update pubsub2dmsenhance.py

--- pubsub2dmsenhance.py
+++ pubsub2dmsenhance.py
@@ -1,43 +1,24 @@
-from google.cloud import pubsub_v1
-from kafka import KafkaProducer
-import time
+1.演示通过快照转发历史数据
 
-# 订阅参数
-project_id = "pubsub-connect-kafka"
-subscription_names = ["pubsub2dws_subscription1", "pubsub2dws_subscription2"]
-# Kafka Broker 信息
-kafka_bootstrap_servers = '192.1.0.127:9092'
-# Pub/Sub to Kafka topic mapping
-topic_mapping = {
-    "pubsub2dws_subscription1": "kafka_topic1",
-    "pubsub2dws_subscription2": "kafka_topic2",
-}
+[gcloud]
+--在PubSub创建一个topic接收Kafka的消息，这个命令跑完之后PubSub有了Topic
+gcloud pubsub topics create pubsub2dws_getfromkafka
 
-def receive_messages(subscription_name):
-    subscriber = pubsub_v1.SubscriberClient()
-    subscription_path = subscriber.subscription_path(project_id, subscription_name)
+--在PubSub创建一个订阅接收从PubSub创建的Topic发送来的消息，这个命令跑完之后PubSub有了subscription
+gcloud pubsub subscriptions create pubsub2dws_subscription --topic=pubsub2dws_getfromkafka
 
-    def callback(message):
-        # 处理接收到的消息
-        message_data = message.data.decode('utf-8')
-        print(f"Received message on {subscription_name}: {message_data}")
-        # 发送消息到Kafka
-        send_to_kafka(topic_mapping[subscription_name], message_data)
-        message.ack()
+[shell]
+--在本地kafka创建一个消费组 连接上topic
+cd C:\kafka\kafka_2.12-3.7.1
+bin\windows\kafka-console-consumer.bat --bootstrap-server 192.1.0.127:9092 --group PubSubConsumerGroup --topic pubsub2dws_kafka --from-beginning 
 
-    subscriber.subscribe(subscription_path, callback=callback)
-    print(f"Listening for messages on {subscription_path}...")
+连接使用数据
+expected message test 1
+expected message test 2
+expected message test 3
+expected message test 4
 
-def send_to_kafka(kafka_topic, message):
-    # 编写代码将消息发送到Kafka
-    producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
-    producer.send(kafka_topic, message.encode('utf-8'))
-    producer.flush()
-    producer.close()
+--查看消费组在本地
+消费情况
 
-if __name__ == '__main__':
-    for subscription_name in subscription_names:
-        receive_messages(subscription_name)
-    # 持续监听消息
-    while True:
-        time.sleep(5)  # 使用 time 模块中的 sleep 函数
+2.演示消费数据同步，手动发送消息
